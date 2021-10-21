64c2440b7f2c5f1e10b7348c79a5820fa4407dcf
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-10-21 10:58:05 -0400

    email function

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -7,3 +7,18 @@ function switchtheme(){
     document.getElementById("body").classList.toggle('body');
     document.getElementById("body").classList.toggle('switch');
 }
+
+function send()
+{
+    emailjs.send("service_gg3vqvw","template_lzvimyq",{
+        name: document.querySelector("#name").value,
+        tech: document.querySelector("#service").value,
+        email: document.querySelector("#email").value
+        }).then(response=>{
+            console.log("Success", response.status);
+        },(error)=>{
+            console.log(error);
+        }) ;
+}
+
+document.querySelector("#submit").addEventListener('click',send);
